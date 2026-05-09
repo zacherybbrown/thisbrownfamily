8074ac48d0fcc93e340c525682b68cd98612e8c6
zacherybbrown <zacherybbrown@gmail.com>
2026-05-08 22:50:06 -0600

    e

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/thisbrownfamily/', // Only needed for GitHub Pages
   plugins: [react()],
+  base: '/thisbrownfamily/', // Your repo name
 })
